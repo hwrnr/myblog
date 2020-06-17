00b1f3d1c9c4c555c20a369f2999fb731b63ad0d
hwrnr <hawerner.hawerner@gmail.com>
2020-06-17 23:49:58 +0200

    Initial commit

---
+++ content/pages/home.md
@@ -0,0 +1,7 @@
+title: Hawerner
+date: 21.04.2019
+URL:
+save_as: index.html
+
+##Linux user, Hacker, Security Researcher
+##Member of <span style="color:#66F;"> [Tilda Center](https://tilda.center/) </span>
---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'hawerner'
+SITENAME = 'Dark Penguin'
+SITEURL = ''
+INDEX_SAVE_AS = 'blog/index.html'
+
+PATH = 'content'
+
+#  INDEX_SAVE_AS=blog/index.html
+DELETE_OUTPUT_DIRECTORY = True
+
+TIMEZONE = 'Europe/Belgrade'
+
+DEFAULT_LANG = 'en'
+
+THEME='../dark-penguin'
+
+MENUITEMS = (
+        ("Blog", "/blog"),
+        ("Resume", "/resume"),
+        )
+
+
+DISPLAY_PAGES_ON_MENU=False
+DISPLAY_MENU=True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+FOOTERITEMS = (
+        ("GitHub", "https://github.com/hwrnr"),
+        )
+FOOTERITEMS += SOCIAL
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
